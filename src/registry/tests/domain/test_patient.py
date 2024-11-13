9d79409c6c371c61db27bccc3dc8f781170cc65b
koskedk <koskedk@live.com>
2024-11-13 21:03:00 +0300

    patient tests fix

--- src/registry/tests/domain/test_patient.py
+++ src/registry/tests/domain/test_patient.py
@@ -4,7 +4,6 @@ from registry.domain.patient import Patient
 
 
 def test_patient_init():
-    patient = Patient(99, "Johnny Doe", datetime(2001, 2, 2))
-    assert patient.id == 99
+    patient = Patient("Johnny Doe", datetime(2001, 2, 2))
     assert patient.name == "Johnny Doe"
     assert patient.dob == datetime(2001, 2, 2)
