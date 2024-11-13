8003557ccee19da1a34bdb2f9c64f106603d3cd4
koskedk <koskedk@live.com>
2024-11-13 20:52:41 +0300

    adding base opts

--- src/shared/shared/domain/money.py
+++ src/shared/shared/domain/money.py
@@ -1,9 +1,23 @@
 from dataclasses import dataclass
 
-from shared.domain.common import ValueObject
+from shared.domain.common import ValueObject, BusinessError
 
 
 @dataclass(frozen=True, order=True)
 class Money(ValueObject):
     amount: float
     currency: str
+
+    def __post_init__(self):
+        if self.amount < 0:
+            raise InvalidAmountError(self.amount)
+
+        object.__setattr__(self, "currency", self.currency.upper())
+
+    def __str__(self):
+        return f"{self.amount:,.2f} {self.currency}"
+
+
+class InvalidAmountError(BusinessError):
+    def __init__(self, amount):
+        super().__init__(f"Invalid amount {amount} ,cannot be less than zero")
--- src/shared/tests/domain/test_money.py
+++ src/shared/tests/domain/test_money.py
@@ -1,7 +1,21 @@
-from shared.domain.money import Money
+import pytest
+
+from shared.domain.money import Money, InvalidAmountError
 
 
 def test_money_init():
-    kes = Money(1, "kes")
-    assert kes.amount == 1
-    assert kes.currency == "kes"
+    kes = Money(1009.12, "kes")
+    assert kes.amount == 1009.12
+    assert kes.currency == "KES"
+    print(f"\n{kes}")
+
+
+def test_money_currency_format():
+    usd = Money(147000.50, "usd")
+    assert usd.currency == "USD"
+    print(f"\n{usd}")
+
+
+def test_money_valid_only():
+    with pytest.raises(InvalidAmountError) as ex:
+        Money(-1, "kes")
